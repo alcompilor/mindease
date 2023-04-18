997ff0f2e056d6381e23bd4bb99ab7877cb4c373
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2023-04-18 19:04:46 +0200

    app.py first commit

---
+++ src/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Flask module."""
+
+from pathlib import Path
+from flask import Flask, render_template
+
+ROOT_DIR = Path(__file__).parent.parent  # getting root dir path
+STATIC_DIR = (ROOT_DIR).joinpath('static')  # generating static dir path
+TEMPLATES_DIR = (ROOT_DIR).joinpath(
+    'templates')  # generating templates dir path
+
+
+app = Flask(__name__,
+            static_folder=STATIC_DIR,
+            template_folder=TEMPLATES_DIR)  # init flask app
+
+
+@app.route("/")  # route
+def home_page():
+    """Route for home page."""
+    data = {"doc_title": "Home | Mindease"}
+    return render_template("index.html", data=data)
+
+
+@app.route("/register")  # route
+def register_page():
+    """Route for account registration page."""
+    data = {"doc_title": "Register | Mindease"}
+    return render_template("register.html", data=data)
+
+
+@app.route("/thankyou")  # TEMPORARY route
+def comingsoon_page():
+    """Route for coming soon page."""
+    data = {"doc_title": "Thank You | Mindease"}
+    return render_template("comingsoon.html", data=data)
