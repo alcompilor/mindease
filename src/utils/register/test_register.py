b4b564063bb1acff1f56db5bed66216d003a69e1
Mohammed <alkatebmohammed383@gmail.com>
2023-04-19 15:39:34 +0200

    Register (#10)
    * Backend Register
    
    * register.py
    
    * Updated register
    
    ---------
    
    Co-authored-by: mohammed-alkateb <alkatebmohammed383@gmail,com>

---
+++ src/utils/register/test_register.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" unittest Register module."""
+
+import unittest
+from src.utils.register.register import Register
+from src.utils.db_connection.db_connection import DBConnection
+
+
+class TestRegister(unittest.TestCase):
+    """Test Register."""
+
+    def test_register_user(self):
+        """Test register_user Function."""
+
+        user = {
+            'first_name': 'Mohammed',
+            'last_name': 'Alkateb',
+            'email': 'jackreacherssss@gmail.com',
+            'password': 'pass1234',
+            'age': 30,
+            'gender': 'male'
+        }
+
+        conn = DBConnection()
+        cursor = conn.cnx.cursor()
+        param = (user['email'],)
+
+        query = 'SELECT * FROM User WHERE email=%s'
+        cursor.execute(query, param)
+        row = cursor.fetchone()
+        self.assertTrue(row is None or
+            len(row) == 0)
+
+        reg = Register(user)
+        user_reg = reg.register_user()
+
+        self.assertEqual(user_reg, {'registration_succeeded': True}, 'Registration failed')
