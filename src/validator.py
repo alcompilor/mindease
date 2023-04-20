610831f2f9a5ee7006b0b1c3126b3ed71d4e77da
Ahmed Abbasi <90004475+bigcbull@users.noreply.github.com>
2023-04-20 23:38:12 +0200

    App (#20)
    * app.py first commit
    
    * Encryption method added to app.py
    
    * merge conflict fix
    
    * updated app.py + added form validator
    
    * updated validator
    
    ---------
    
    Co-authored-by: Ahmed Abbasi <ahmed-abbasi@outlook.com>
    Co-authored-by: mohammed-alkateb <alkatebmohammed383@gmail,com>

--- src/validator.py
+++ src/validator.py
@@ -11,28 +11,29 @@ class ValidateRegister(Form):
     """Register Validator to validate client side register form."""
 
     first_name = StringField('First Name', validators=[validators.Length(
-        min=1, max=35), validators.DataRequired()])
+        min=1, max=35), validators.DataRequired()], id="first-name")
 
     last_name = StringField('Last Name', validators=[validators.Length(
-        min=1, max=35), validators.DataRequired()])
+        min=1, max=35), validators.DataRequired()], id="last-name")
 
     email = EmailField('Email', validators=[validators.Length(
-        min=1, max=254), validators.Email(), validators.DataRequired()])
+        min=1, max=254), validators.Email(), validators.DataRequired()],
+        id="email")
 
     password = PasswordField('New Password', validators=[
         validators.DataRequired(),
         validators.EqualTo('password_confirm', message='Passwords must match')
-    ])
+    ], id="password")
 
     password_confirm = PasswordField('Repeat Password', validators=[
-        validators.DataRequired()])
+        validators.DataRequired()], id="password-confirm")
 
     gender = SelectField('Gender', choices=[('male', 'Male'),
                                             ('female', 'Female')],
-                         validators=[validators.DataRequired()])
+                         validators=[validators.DataRequired()], id="gender")
 
     birth = DateField('Date of Birth',
-                      validators=[validators.DataRequired()])
+                      validators=[validators.DataRequired()], id="date-birth")
 
     accept_tos = BooleanField(validators=[
-                              validators.DataRequired()])
+                              validators.DataRequired()], id="tos")
