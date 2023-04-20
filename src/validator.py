cb320e758e2b2cf1069927b612dd936b95cfb951
Ahmed Abbasi <90004475+bigcbull@users.noreply.github.com>
2023-04-21 00:56:20 +0200

    App (#22)
    * app.py first commit
    
    * Encryption method added to app.py
    
    * merge conflict fix
    
    * updated app.py + added form validator
    
    * updated validator
    
    * added flask secret key
    
    * updated validator.py & app.py
    
    ---------
    
    Co-authored-by: Ahmed Abbasi <ahmed-abbasi@outlook.com>
    Co-authored-by: mohammed-alkateb <alkatebmohammed383@gmail,com>

--- src/validator.py
+++ src/validator.py
@@ -3,37 +3,91 @@
 
 """Validator module."""
 
-from wtforms import (Form, BooleanField, StringField, PasswordField,
-                     EmailField, SelectField, DateField, validators)
+from wtforms import (
+    Form,
+    BooleanField,
+    StringField,
+    PasswordField,
+    EmailField,
+    SelectField,
+    DateField,
+    validators,
+)
 
 
 class ValidateRegister(Form):
     """Register Validator to validate client side register form."""
 
-    first_name = StringField('First Name', validators=[validators.Length(
-        min=1, max=35), validators.DataRequired()], id="first-name")
+    first_name = StringField(
+        "First Name",
+        validators=[
+            validators.Length(min=1, max=35, message="First Name is invalid"),
+            validators.DataRequired(message="First Name is required"),
+        ],
+        id="first-name",
+    )
 
-    last_name = StringField('Last Name', validators=[validators.Length(
-        min=1, max=35), validators.DataRequired()], id="last-name")
+    last_name = StringField(
+        "Last Name",
+        validators=[
+            validators.Length(min=1, max=35, message="Last Name is invalid"),
+            validators.DataRequired(message="Last Name is required"),
+        ],
+        id="last-name",
+    )
 
-    email = EmailField('Email', validators=[validators.Length(
-        min=1, max=254), validators.Email(), validators.DataRequired()],
-        id="email")
+    email = EmailField(
+        "Email",
+        validators=[
+            validators.Length(min=1, max=254, message="Email is invalid"),
+            validators.Email(message="Email is invalid"),
+            validators.DataRequired(message="Email is required"),
+        ],
+        id="email",
+    )
 
-    password = PasswordField('New Password', validators=[
-        validators.DataRequired(),
-        validators.EqualTo('password_confirm', message='Passwords must match')
-    ], id="password")
+    password = PasswordField(
+        "New Password",
+        validators=[
+            validators.DataRequired(message="Password is required"),
+            validators.EqualTo("password_confirm",
+                               message="Passwords must match"),
+            validators.Length(
+                min=8, max=50,
+                message="Password must be between 8 and 50 characters"
+            ),
+        ],
+        id="password",
+    )
 
-    password_confirm = PasswordField('Repeat Password', validators=[
-        validators.DataRequired()], id="password-confirm")
+    password_confirm = PasswordField(
+        "Repeat Password",
+        validators=[
+            validators.DataRequired(
+                message="Password confirmation sis required")
+        ],
+        id="password-confirm",
+    )
 
-    gender = SelectField('Gender', choices=[('male', 'Male'),
-                                            ('female', 'Female')],
-                         validators=[validators.DataRequired()], id="gender")
+    gender = SelectField(
+        "Gender",
+        choices=[("male", "Male"), ("female", "Female")],
+        validators=[validators.DataRequired(message="Gender is required")],
+        id="gender",
+    )
 
-    birth = DateField('Date of Birth',
-                      validators=[validators.DataRequired()], id="date-birth")
+    birth = DateField(
+        "Date of Birth",
+        validators=[validators.DataRequired(
+            message="Date of birth is required")],
+        id="date-birth",
+        format="%Y-%m-%d",
+    )
 
-    accept_tos = BooleanField(validators=[
-                              validators.DataRequired()], id="tos")
+    accept_tos = BooleanField(
+        validators=[
+            validators.DataRequired(
+                message="You must accept terms & conditions")
+        ],
+        id="tos",
+    )
