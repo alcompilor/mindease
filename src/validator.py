85072d0bf30108b8a155acc6574834eabc4e53ed
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2023-04-22 00:47:13 +0200

    added login validator in validator.py

--- src/validator.py
+++ src/validator.py
@@ -12,7 +12,22 @@ from wtforms import (
     SelectField,
     DateField,
     validators,
+    ValidationError,
 )
+import mysql.connector
+from src.utils.db_connection.db_connection import DBConnection
+
+
+def validate_email_db(form, field):
+    """Check whether email exists in database. Used in Login Validator."""
+    try:
+        database = DBConnection()
+        query = "SELECT user_id FROM User WHERE email = %s"
+        database.cursor.execute(query, field.data)
+        database.cursor.fetchone()
+
+    except mysql.connector.Error as err:
+        raise ValidationError('Email does not exist') from err
 
 
 class ValidateRegister(Form):
@@ -25,6 +40,7 @@ class ValidateRegister(Form):
             validators.DataRequired(message="First Name is required"),
         ],
         id="first-name",
+        render_kw={"placeholder": "John"},
     )
 
     last_name = StringField(
@@ -34,6 +50,7 @@ class ValidateRegister(Form):
             validators.DataRequired(message="Last Name is required"),
         ],
         id="last-name",
+        render_kw={"placeholder": "Smith"},
     )
 
     email = EmailField(
@@ -44,6 +61,7 @@ class ValidateRegister(Form):
             validators.DataRequired(message="Email is required"),
         ],
         id="email",
+        render_kw={"placeholder": "john.smith@gmail.com"},
     )
 
     password = PasswordField(
@@ -58,6 +76,7 @@ class ValidateRegister(Form):
             ),
         ],
         id="password",
+        render_kw={"placeholder": "Enter a password"},
     )
 
     password_confirm = PasswordField(
@@ -67,6 +86,7 @@ class ValidateRegister(Form):
                 message="Password confirmation sis required")
         ],
         id="password-confirm",
+        render_kw={"placeholder": "Re-enter password"},
     )
 
     gender = SelectField(
@@ -90,4 +110,33 @@ class ValidateRegister(Form):
                 message="You must accept terms & conditions")
         ],
         id="tos",
+    )
+
+
+class ValidateLogin(Form):
+    """Login Validator to validate client side login form."""
+
+    email = EmailField(
+        "Email",
+        validators=[
+            validators.Length(min=1, max=254, message="Email is invalid"),
+            validators.Email(message="Email is invalid"),
+            validators.DataRequired(message="Email is required"),
+            validate_email_db,
+        ],
+        id="email",
+        render_kw={"placeholder": "Your email"},
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[
+            validators.DataRequired(message="Password is required"),
+            validators.Length(
+                min=8, max=50,
+                message="Password is between 8 and 50 characters"
+            ),
+        ],
+        id="password",
+        render_kw={"placeholder": "Your password"},
     )
