013d422f75bdea8b93f78f4e2f37d07872ce529f
Ahmed Abbasi <90004475+bigcbull@users.noreply.github.com>
2023-05-01 21:18:29 +0200

    Frontend (#73)
    * static + templates first commit
    
    * updated static + templates
    
    * updated register ui
    
    * updated static + templates
    
    * updated static + templates
    
    * updated static + templates
    
    * updated static + templates
    
    * updated register
    
    * updated register
    
    * updated register
    
    * first release static + templates
    
    * updated register.html
    
    * fixed messages in register.html
    
    * fixed datepicker interval
    
    * added login.html
    
    * unified register and login css + added login DOM
    
    * changed login pic + made login btn load faster
    
    * UI changes to match logged in/logged out status
    
    * renamed 'dashboard' to 'my space'
    
    * added checkup + updated space
    
    * updated my space + checkup
    
    * added meditation timer + updated myspace and its css
    
    * myspace section is finalized
    
    * some css tweaks to myspace section
    
    * fixed metrics.js
    
    * restructured my space templating
    
    * added journals
    
    * added checkup form + journal form
    
    * added favicon + edited space menu to temp hide account link
    
    * added stronger pass validation for registeration + edited myspace css
    
    ---------
    
    Co-authored-by: Ahmed Abbasi <ahmed-abbasi@outlook.com>

---
+++ static/js/space/journals.js
@@ -0,0 +1,17 @@
+// Fetch all the details element.
+const details = document.querySelectorAll("details");
+
+// Add the onclick listeners.
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
+
+const card = document.getElementById("journal-card");
+if (!card) document.getElementById("journals-none").style.display = "block";
