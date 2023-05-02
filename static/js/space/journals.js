ec02a973b9a1c66168d1de62256de0737480fbd1
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2023-05-02 20:24:43 +0200

    added journals to frontend

--- static/js/space/journals.js
+++ static/js/space/journals.js
@@ -1,10 +1,46 @@
 const saveBtn = document.getElementById("journal-btn");
 const journalForm = document.getElementById("journal-form");
-const journalSubmissionDate = document.getElementById("journal-submission-date");
+const journalSubmissionDate = document.getElementById(
+  "journal-submission-date"
+);
+
+const journalSearchInput = document.getElementById("journal-search-input");
+const journalSearchButton = document.getElementById("journal-searchbtn");
+
+const cardsContainer = document.getElementById("myspace-journals-cards");
+
+const reversedJournals = journals.reverse();
 
 saveBtn.addEventListener("click", () => {
   journalSubmissionDate.value = new Date().toLocaleDateString("sv-SE");
   journalForm.submit();
+});
+
+const createCard = (title, content, date) => {
+  const cardDiv = document.createElement("div");
+  const cardDetails = document.createElement("details");
+  const cardSummary = document.createElement("summary");
+  const cardPara = document.createElement("p");
+
+  cardDiv.id = "journal-card";
+
+  cardSummary.innerHTML = `${title} <span id="journal-date">${date}</span>`;
+  cardPara.textContent = content;
+
+  cardDetails.append(cardSummary, cardPara);
+  cardDiv.appendChild(cardDetails);
+
+  cardsContainer.appendChild(cardDiv);
+};
+
+reversedJournals.forEach((element) => {
+  const title = element["journal_content"]["title"];
+  const content = element["journal_content"]["content"];
+  const date = new Date(element["journal_content"]["date"]).toLocaleDateString(
+    "sv-SE"
+  );
+
+  createCard(title, content, date);
 });
 
 // Fetch all the details element.
