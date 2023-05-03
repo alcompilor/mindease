d130dcae85be82dad35c9038b863e42d81df1e87
Nour <mohamednourhumeidi@gmail.com>
2023-05-03 10:39:21 +0200

    fixed the %s and adjusted a litlle in register method and the other methods return expected values

--- src/utils/checkup/checkup.py
+++ src/utils/checkup/checkup.py
@@ -16,47 +16,65 @@ class CheckUp:
         self.conn.close()
 
 
-    def fetch_checkup(self, date):
+    def fetch_checkup(self, checkup_id):
         """Fetches the checkup for a specific date from the database."""
+        try:
+            db = DBConnection()
+            query = "SELECT * FROM checkup WHERE checkup_id = %s ORDER BY id DESC LIMIT 1"
+            db.cursor.execute(query, checkup_id)
+            result = db.cursor.fetchone()
+            if checkup_id < 1 or checkup_id > 30:
+                checkup_id = 1
+            todays_checkup = result +1
+            
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
         
-        db = DBConnection()
-        query = "SELECT * FROM checkup_answer WHERE date = ?"
-        db.cursor.execute(query, date)
-        result = db.cursor.fetchone()
         db.cursor.close()
         db.cnx.close()
-        return {"Today's checkup: " : result[0] if result else None}
+    
+        return {"Todays_checkup: " : todays_checkup}
     
 
     def fetch_latest_check(self, checkup_id):
         """Fetches the latest checkup answer for a specific checkup ID."""
         
         db = DBConnection()
-        query = "SELECT * FROM checkup_answer WHERE checkup_id = ? ORDER BY id DESC LIMIT 1"
+        query = "SELECT * FROM checkup_answer WHERE checkup_id = %s ORDER BY id DESC LIMIT 1"
         db.cursor.execute(query, checkup_id)
         result = db.cursor.fetchone()
-        return {"The answer for the checkup is " : result}
+    
+        return {"latest_checkup" : result}
     
 
     def check_answer(self, answer_id):
         """Check if the checkup answer with given ID was stored in the last 24 hours or more."""
         
-        db = DBConnection()
-        query = "SELECT created_at FROM checkup_answer WHERE id = ?"
-        db.cursor.execute(query, answer_id)
-        answer_date = datetime.datetime.strptime(self.cursor.fetchone()[0], "%Y-%m-%d %H:%M:%S.%f")
+        try:
+            db = DBConnection()
+            query = "SELECT created_at FROM checkup_answer WHERE id = %s"
+            db.cursor.execute(query, answer_id)
+            answer_date = datetime.datetime.strptime(self.cursor.fetchone()[0], "%Y-%m-%d %H:%M:%S.%f")
+            success = True
+        
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            success = False
+        
         db.cursor.close()
         db.cnx.close()
-        return {datetime.datetime.now() - answer_date >= datetime.timedelta(days=1)}
+        checked_answer = datetime.datetime.now() - answer_date >= datetime.timedelta(days=1)
+        
+        return {"checked_answer" : checked_answer}
 
 
-    def register_checkup(self, checkup_id, answer):
+    def register_checkup(self, checkup_id,user_id, answer, answer_date):
         """Registers a checkup answer for a specific checkup ID."""
         
         try:
             db = DBConnection()
-            query = "INSERT INTO checkup_answer (checkup_id, answer) VALUES (?, ?)"
-            data = (checkup_id, answer)
+            query = "INSERT INTO checkup_answer (checkup_id, user_id, answer, answer_date) VALUES (%s, %s, %s, %s)"
+            data = (checkup_id,user_id, answer,answer_date)
             if answer < 1 or answer > 5:
                 raise ValueError("Answer must be a number between 1 and 5.")
             db.cursor.execute(query, data)
@@ -69,19 +87,4 @@ class CheckUp:
         
         db.cursor.close()
         db.cnx.close()
-        return {"Answer Regsitered" : registered}
-
-
-"""""
-def collect_checkup(self, user_id):
-    db = DBConnection()
-    cursor = db.cnx.cursor()
-    query = ("SELECT * FROM checkup WHERE id = %s")
-    checkup_id = 1
-    cursor.execute(query, (checkup_id))
-    checkup_data = cursor.fetchone()
-    
-    cursor.close()
-    db.cnx.close()
-    print(checkup_data)
-"""""
+        return {"answer_regsitered" : registered}
