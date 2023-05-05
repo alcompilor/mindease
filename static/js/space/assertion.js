e9c8aa95b71789641b21a22a68cc27f8393dc875
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2023-05-05 17:26:53 +0200

    tweaked assertion to fetch it from backend in order to avoid cors issues

--- static/js/space/assertion.js
+++ static/js/space/assertion.js
@@ -1,24 +1,5 @@
-const assertionEl = document.getElementById("assertion"); // assertion container
 const newAssertion = document.getElementById("assertion-new"); // new assertion btn
 
-// Async function to get assertion from external api
-async function reqAssertion() {
-  const assertion = await fetch("https://www.affirmations.dev", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-    mode: "no-cors",
-  }).then((response) => response.json());
-  return await assertion.affirmation;
-};
-
-// Function to render assertion fetched from the async function
-const renderAssertion = () => {
-  reqAssertion().then((res) => {
-    assertionEl.textContent = res;
-  });
-};
-
-newAssertion.addEventListener("click", renderAssertion); // event listener for assertion btn
-renderAssertion(); // render assertion on first page load
+newAssertion.addEventListener("click", () => {
+  window.location.reload();
+}); // event listener for assertion btn
