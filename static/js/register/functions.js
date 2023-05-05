8990ebc3fab295c3a8b1170cf73b608c7ca40068
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2023-05-05 15:37:57 +0200

    cleaned js code + tweaked comments

--- static/js/register/functions.js
+++ static/js/register/functions.js
@@ -1,13 +1,16 @@
+// Function to get max date and min date based on age reqs
+
 export function dateLimit(type) {
   switch (type) {
-    case "max":
+    case "max": // Based on minimum age of 13
       const [maxDate] = new Date(
         new Date().setFullYear(new Date().getFullYear() - 13)
       )
         .toISOString()
         .split("T");
       return maxDate;
-    case "min":
+
+    case "min": // Based on a max age of 90
       const [minDate] = new Date(
         new Date().setFullYear(new Date().getFullYear() - 90)
       )
--- static/js/space/assertion.js
+++ static/js/space/assertion.js
@@ -1,22 +1,24 @@
-const assertionEl = document.getElementById("assertion");
-const newAssertion = document.getElementById("assertion-new");
+const assertionEl = document.getElementById("assertion"); // assertion container
+const newAssertion = document.getElementById("assertion-new"); // new assertion btn
 
+// Async function to get assertion from external api
 async function reqAssertion() {
   const assertion = await fetch("https://www.affirmations.dev", {
     method: "GET",
     headers: {
       Accept: "application/json",
     },
+    mode: "no-cors",
   }).then((response) => response.json());
   return await assertion.affirmation;
-}
+};
 
+// Function to render assertion fetched from the async function
 const renderAssertion = () => {
   reqAssertion().then((res) => {
     assertionEl.textContent = res;
   });
 };
 
-newAssertion.addEventListener("click", renderAssertion);
-
-renderAssertion();
+newAssertion.addEventListener("click", renderAssertion); // event listener for assertion btn
+renderAssertion(); // render assertion on first page load
