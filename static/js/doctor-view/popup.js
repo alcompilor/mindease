af79441b67374762455372c9dc81e9d3f30ec3fb
Mohammed <alkatebmohammed383@gmail.com>
2023-05-07 18:55:14 +0200

    Frontend doctorview (#139)

--- static/js/doctor-view/popup.js
+++ static/js/doctor-view/popup.js
@@ -1,12 +1,6 @@
 let popup = document.getElementById("popup");
 let blackLayer = document.getElementById("black-layer");
 
-let averageElement = document.getElementById("answer_average");
-let answerAverage = averageElement.dataset.answerAverage;
-
-let element = document.querySelector('.progress-circle');
-element.style.setProperty('--percent', answerAverage);
-
 function openPopup() {
     popup.classList.add("open-popup");
     blackLayer.style.display = "block";
